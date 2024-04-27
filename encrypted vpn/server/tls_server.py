6b8e67574b86a35a3807df939fef5d05e708523d
wetzelTanner <140431902+wetzelTanner@users.noreply.github.com>
2024-04-27 02:16:59 -0400

    fully working encryption, updated tcp server

--- encrypted vpn/server/tls_server.py
+++ encrypted vpn/server/tls_server.py
@@ -27,46 +27,35 @@ os.system("ip link set dev {} up".format(ifname))
 
 # provide context for the ssl connection
 context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-context.load_cert_chain(certfile="./server-cert.pem", keyfile="./server-key.pem")
+context.load_cert_chain(certfile="./newservcert.pem", keyfile="./server-key.pem")
 
 # set up the socket
 IP_A = "0.0.0.0"
 PORT = 9090
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
 sock.bind((IP_A, PORT))
-sock.listen(5)  # Listen for incoming connections
+sock.listen(5)  # listen for incoming connections
 
-# Wrap the socket with SSL
+# wrap the socket with SSL
 tls_sock = context.wrap_socket(sock, server_side=True)
+conn, _ = tls_sock.accept()
 
-fds = [tls_sock, tun]
+fds = [tun, conn]
 
 while True:
     ready, _, _ = select.select(fds, [], [])
 
     for fd in ready:
-        if fd is tls_sock:
-            # New connection
-            conn, _ = tls_sock.accept()
-            fds.append(conn)  # Add the new connection to the list of fds
+        if fd is conn:
+            # send to the TUN interface
+            packet = conn.recv(2048)
+            pkt = IP(packet)
+            print("From socket <==: {} --> {}".format(pkt.src, pkt.dst))
+            os.write(tun, packet)
 
-        elif fd is tun:
+        if fd is tun:
             # send to connected client
             packet = os.read(tun, 2048)
             pkt = IP(packet)
             print("From tun      ==>: {} --> {}".format(pkt.src, pkt.dst))
-            for conn in fds:
-                if conn is not tls_sock and conn is not tun:
-                    conn.send(packet)
-
-        else:
-            # send to the TUN interface
-            packet = conn.recv(2048)
-            if not packet:
-                conn.close()
-                fds.remove(conn)
-            else:
-                pkt = IP(packet)
-                print("From socket <==: {} --> {}".format(pkt.src, pkt.dst))
-                print(pkt.summary())
-                os.write(tun, packet)
+            conn.send(packet)
--- unencrypted vpn/tcp/tcp_server.py
+++ unencrypted vpn/tcp/tcp_server.py
@@ -24,40 +24,31 @@ print("Created TUN interface: {}".format(ifname))
 os.system("ip addr add 10.0.53.1/24 dev {}".format(ifname))
 os.system("ip link set dev {} up".format(ifname))
 
-# set up the TCP socket
+# set up the socket
 IP_A = "0.0.0.0"
 PORT = 9090
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind((IP_A, PORT))
 sock.listen(5)  # listen for incoming connections
 
-fds = [sock, tun]
+conn, _ = sock.accept() # accept connection and add to fds
+
+fds = [tun, conn]
 
 while True:
     ready, _, _ = select.select(fds, [], [])
 
     for fd in ready:
-        if fd is sock:
-            # new connection
-            conn, _ = sock.accept()
-            fds.append(conn)  # Add the new connection to the list of fds
+        if fd is conn:
+            # send to the TUN interface
+            packet = conn.recv(2048)
+            pkt = IP(packet)
+            print("From socket <==: {} --> {}".format(pkt.src, pkt.dst))
+            os.write(tun, packet)
 
-        elif fd is tun:
-            # send data to connected client
+        if fd is tun:
+            # send to connected client
             packet = os.read(tun, 2048)
             pkt = IP(packet)
             print("From tun      ==>: {} --> {}".format(pkt.src, pkt.dst))
-            for conn in fds:
-                if conn is not sock and conn is not tun:
-                    conn.sendall(packet)
-
-        else:
-            # send data to the TUN interface
-            packet = conn.recv(2048)
-            if not packet:
-                conn.close()
-                fds.remove(conn)
-            else:
-                pkt = IP(packet)
-                print("From socket <==: {} --> {}".format(pkt.src, pkt.dst))
-                os.write(tun, packet)
+            conn.send(packet)
